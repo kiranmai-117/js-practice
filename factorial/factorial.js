d84a82f0f88ebd9250a3205d37833fe671dd480d
kiranmai-117 <kiranmailalam@gmail.com>
2025-09-19 12:40:13 +0530

    Create js file for factorial

---
+++ factorial/factorial.js
@@ -0,0 +1,8 @@
+let counter = 1;
+let factorial = 1;
+const input = 0;
+while (counter <= input) {
+  factorial = counter * factorial;
+  counter++;
+}
+console.log("factorial of",input,"is", factorial);
