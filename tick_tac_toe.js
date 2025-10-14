b357f84841f664ece42e7e2418fdf127de468914
kiranmai-117 <kiranmailalam@gmail.com>
2025-10-14 12:31:10 +0530

    Update tick_tac_toe.js

--- tick_tac_toe.js
+++ tick_tac_toe.js
@@ -1,84 +1,82 @@
-function count(position, value, countX, countO) {
-  const row = parseInt(position[0]);
-  const col = parseInt(position[2]);
+const PLY1_SYL = "🔥";
+const PLY2_SYL = "🌊";
 
+function count(position, value, countX, countO) {
+  const row = parseInt(position[0][0]);
+  const col = parseInt(position[0][2]);
   if (row > 0) {
-    countX[row - 1] = value === "🔥" ? countX[row - 1] + 1 : 0;
-    countO[row - 1] = value === "🌊" ? countO[row - 1] + 1 : 0;
+    countX[row - 1] = value === PLY1_SYL ? countX[row - 1] + 1 : 0;
+    countO[row - 1] = value === PLY2_SYL ? countO[row - 1] + 1 : 0;
   }
   if (col > 0) {
-    countX[col + 2] = value === "🔥" ? countX[col + 2] + 1 : 0;
-    countO[col + 2] = value === "🌊" ? countO[col + 2] + 1 : 0;
+    countX[col + 2] = value === PLY1_SYL ? countX[col + 2] + 1 : 0;
+    countO[col + 2] = value === PLY2_SYL ? countO[col + 2] + 1 : 0;
   }
   if (col === row) {
-    countX[6] = value === "🔥" ? countX[6] + 1 : 0;
-    countO[6] = value === "🌊" ? countO[6] + 1 : 0;
+    countX[6] = value === PLY1_SYL ? countX[6] + 1 : 0;
+    countO[6] = value === PLY2_SYL ? countO[6] + 1 : 0;
   }
   if (col === row && row === 2 || Math.abs(col - row) === 2) {
-    countX[7] = value === "🔥" ? countX[7] + 1 : 0;
-    countO[7] = value === "🌊" ? countO[7] + 1 : 0;
+    countX[7] = value === PLY1_SYL ? countX[7] + 1 : 0;
+    countO[7] = value === PLY2_SYL ? countO[7] + 1 : 0;
   }
 }
 
 function isTriple(position, value, countX, countO) {
   count(position, value, countX, countO);
-  console.log("countX", countX);
-  console.log("countO", countO);
-
-  if (countX.includes(3)) {
-    return "🔥";
+  if (countX.includes(3) || countO.includes(3)) {
+    return true;
   }
-  if (countO.includes(3)) {
-    return "🌊";
-  }
+  return false;
 }
 
-function updateBoard(board, row, col, value) {
-  let box = board[row];
-  board[row] = box.slice(0, 3 * col) + value + box.slice(3 * col + 2);
-  return board;
+function updateBoard(board, cell, value) {
+  let box = board[cell[0]];
+  board[cell[0]] = box.slice(0, 3 * cell[1]) + value + box.slice(3 * cell[1] + 2);
 }
 
-function isValidPosition(board, position, value) {
-  let row = parseInt(position[0]);
-  let col = parseInt(position[2]);
-  row = row + (row - 1);
-  col = col + (col - 1);
-  let box = board[row];
-  let place;
-  if (row < 0 || row > 7 || col < 0 || col > 7) {
-    place = prompt("Invalid position enter again:");
-    isValidPosition(board, place, value);
-    return board;
+function isValidCell(board, position, value, cell) {
+  cell.pop();
+  cell.pop();
+  const row = parseInt(position[0][0], 10);
+  const col = parseInt(position[0][2], 10);
+  cell.push(row + (row - 1));
+  cell.push(col + (col - 1));
+  let box = board[cell[0]];
+  if (isNaN(cell[0]) || isNaN(col) || cell[0] < 1 || cell[0] > 7 || cell[1] < 1 || cell[1] > 7) {
+    position.push(prompt("Invalid position enter again:"));
+    position.shift();
+    isValidCell(board, position, value, cell);
   }
 
-  if (box[3 * col] !== " ") {
-    place = prompt("filled enter again:");
-    isValidPosition(board, place, value);
-    return board;
+  if (box[3 * cell[1]] !== " ") {
+    position.push(prompt("Invalid position enter again:"));
+    position.shift();
+    isValidCell(board, position, value, cell);
   }
-  return updateBoard(board, row, col, value);
+  return true;
 }
 
-function turns(board, player1, player2) {
-  let countX = [0, 0, 0, 0, 0, 0, 0, 0];
-  let countO = [0, 0, 0, 0, 0, 0, 0, 0];
+function turns(board, player1, player2, countX, countO) {
+  const cell = [];
   for (let count = 0; count < 9; count++) {
-    const player = count % 2 === 0 ? player1 : player2;
-    const value = player === player1 ? "🔥" : "🌊";
+    const symbol = count % 2 === 0 ? PLY1_SYL : PLY2_SYL;
+    const player = PLY1_SYL === symbol ? player1 : player2;
     console.log(player);
-    const position = prompt("tell the position:");
-    board = isValidPosition(board, position, value);
+    const position = [prompt("tell the position:")];
+
+    if (isValidCell(board, position, symbol, cell)) {
+      updateBoard(board, cell, symbol);
+    }
+
     console.clear();
     console.log(board.join("\n"));
-    const tripleValue = isTriple(position, value, countX, countO);
-    if (tripleValue === "🔥") {
-      return player1;
-    }
-    if (tripleValue === "🌊") {
-      return player2;
+    if (isTriple(position, symbol, countX, countO)) {
+      win(position, symbol, player1, player2, countX, countO);
+      return;
     }
   }
+  win();
 }
 
 function generateLine(row, cols = 18, char = "-|") {
@@ -96,29 +94,34 @@ function generateBoard() {
   return lines;
 }
 
-function win(Winner) {
-  if (Winner === undefined) {
+function win(symbol, player1, player2) {
+  const winner = symbol === PLY1_SYL ? player1 : player2;
+
+  if (winner === undefined) {
     console.log(" 🤝🤝🤝🤝🤝  match draw 🤝🤝🤝🤝🤝");
     console.log(" 👉 if you want to play again \n");
     console.log(" 👉 please press upward arrow and press enter \n");
     return;
   }
-  console.log(" 🏆🏆🏆🏆🏆🏆 🥳 congratulations  ", Winner, " 🥳 🏆🏆🏆🏆🏆🏆\n");
+  console.log(" 🏆🏆🏆🏆🏆🏆 🥳 congratulations  ", winner, " 🥳 🏆🏆🏆🏆🏆🏆\n");
   console.log(" 👉 if you want to play again \n");
   console.log(" 👉 please press upward arrow and press enter \n");
 }
 
-function play() {
-  const players = prompt("enter player names:");
-  const index = players.indexOf(" ");
-  const player1 = players.slice(0, index);
-  const player2 = players.slice(index + 1);
-  console.log(player1, ": '🔥'");
-  console.log(player2, ": '🌊'");
-  let board = generateBoard();
-  console.log(board.join("\n"));
-  const Winner = turns(board, player1, player2);
-  win(Winner);
+function play(board, player1, player2) {
+  const countX = [0, 0, 0, 0, 0, 0, 0, 0];
+  const countO = [0, 0, 0, 0, 0, 0, 0, 0];
+  turns(board, player1, player2, countX, countO);
 }
 
-play();
+function startGame() {
+  const player1 = prompt("enter player1 name:");
+  const player2 = prompt("enter player2 name:");
+  console.log(player1, ": '🔥'");
+  console.log(player2, ": '🌊'");
+  const board = generateBoard();
+  console.log(board.join("\n"));
+  play(board, player1, player2);
+}
+
+startGame();
