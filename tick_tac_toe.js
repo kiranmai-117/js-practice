18e040f93ee901505c7fe46b4e85275f2408b3d4
kiranmai-117 <kiranmailalam@gmail.com>
2025-10-15 10:26:01 +0530

    Update tick_tac_toe.js

--- tick_tac_toe.js
+++ tick_tac_toe.js
@@ -4,30 +4,36 @@ const PLY2_SYL = "🌊";
 function count(position, value, countX, countO) {
   const row = parseInt(position[0][0]);
   const col = parseInt(position[0][2]);
+
   if (row > 0) {
     countX[row - 1] = value === PLY1_SYL ? countX[row - 1] + 1 : 0;
     countO[row - 1] = value === PLY2_SYL ? countO[row - 1] + 1 : 0;
   }
+
   if (col > 0) {
     countX[col + 2] = value === PLY1_SYL ? countX[col + 2] + 1 : 0;
     countO[col + 2] = value === PLY2_SYL ? countO[col + 2] + 1 : 0;
   }
+
   if (col === row) {
     countX[6] = value === PLY1_SYL ? countX[6] + 1 : 0;
     countO[6] = value === PLY2_SYL ? countO[6] + 1 : 0;
   }
+
   if (col === row && row === 2 || Math.abs(col - row) === 2) {
     countX[7] = value === PLY1_SYL ? countX[7] + 1 : 0;
     countO[7] = value === PLY2_SYL ? countO[7] + 1 : 0;
   }
+
 }
 
 function isTriple(position, value, countX, countO) {
   count(position, value, countX, countO);
-  if (countX.includes(3) || countO.includes(3)) {
-    return true;
-  }
-  return false;
+
+  console.log("countX",countX);
+  console.log("countO",countO);
+
+  return countX.includes(3) || countO.includes(3);
 }
 
 function updateBoard(board, cell, value) {
@@ -38,10 +44,13 @@ function updateBoard(board, cell, value) {
 function isValidCell(board, position, value, cell) {
   cell.pop();
   cell.pop();
+
   const row = parseInt(position[0][0], 10);
   const col = parseInt(position[0][2], 10);
+
   cell.push(row + (row - 1));
   cell.push(col + (col - 1));
+
   let box = board[cell[0]];
   if (isNaN(cell[0]) || isNaN(col) || cell[0] < 1 || cell[0] > 7 || cell[1] < 1 || cell[1] > 7) {
     position.push(prompt("Invalid position enter again:"));
@@ -50,29 +59,34 @@ function isValidCell(board, position, value, cell) {
   }
 
   if (box[3 * cell[1]] !== " ") {
-    position.push(prompt("Invalid position enter again:"));
+    position.push(prompt("filled enter again:"));
     position.shift();
     isValidCell(board, position, value, cell);
   }
+
   return true;
 }
 
 function turns(board, player1, player2, countX, countO) {
   const cell = [];
-  for (let count = 0; count < 9; count++) {
-    const symbol = count % 2 === 0 ? PLY1_SYL : PLY2_SYL;
-    const player = PLY1_SYL === symbol ? player1 : player2;
+
+  for (let move = 0; move < 9; move++) {
+
+    const player = move % 2 === 0 ? player1 : player2;
+    const symbol = move % 2 === 0 ? PLY1_SYL : PLY2_SYL;
+
     console.log(player);
     const position = [prompt("tell the position:")];
-
+    
     if (isValidCell(board, position, symbol, cell)) {
       updateBoard(board, cell, symbol);
     }
 
     console.clear();
     console.log(board.join("\n"));
+    
     if (isTriple(position, symbol, countX, countO)) {
-      win(position, symbol, player1, player2, countX, countO);
+      win(symbol, player1, player2);
       return;
     }
   }
@@ -111,16 +125,20 @@ function win(symbol, player1, player2) {
 function play(board, player1, player2) {
   const countX = [0, 0, 0, 0, 0, 0, 0, 0];
   const countO = [0, 0, 0, 0, 0, 0, 0, 0];
+
   turns(board, player1, player2, countX, countO);
 }
 
 function startGame() {
   const player1 = prompt("enter player1 name:");
   const player2 = prompt("enter player2 name:");
-  console.log(player1, ": '🔥'");
-  console.log(player2, ": '🌊'");
+
+  console.log(`${player1}: ${PLY1_SYL}`);
+  console.log(`${player2}: ${PLY2_SYL}`);
+
   const board = generateBoard();
   console.log(board.join("\n"));
+
   play(board, player1, player2);
 }
 
