4ba2cc7148ae33dd4c72dce238ddb0255db6b452
kiranmai-117 <kiranmailalam@gmail.com>
2025-10-15 10:26:20 +0530

    Update tick_tac_toe.js

--- tick_tac_toe.js
+++ tick_tac_toe.js
@@ -29,10 +29,6 @@ function count(position, value, countX, countO) {
 
 function isTriple(position, value, countX, countO) {
   count(position, value, countX, countO);
-
-  console.log("countX",countX);
-  console.log("countO",countO);
-
   return countX.includes(3) || countO.includes(3);
 }
 
