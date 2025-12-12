f07061f1e5d5f097b2297da105a779ac0506f759
Kiranmai Lalam <kiranmailalam@Kiranmais-MacBook-Pro.local>
2025-12-12 19:50:08 +0530

    consecutive pairs

--- generator/consecutive.js
+++ generator/consecutive.js
@@ -1,8 +1,8 @@
 function* consecutive(numbers) {
-  let index = 0;
-  while (true) {
-    yield [numbers[index++], numbers[index]];
+  for (let index = 0; index < numbers.length - 1; index++) {
+    yield [numbers[index], numbers[index + 1]];
   }
 }
 
 const x = consecutive([1, 2, 3, 4, 5]);
+console.log([...x]);
