7945a8211f063830b0b7c3aef9f6e802c20bdc2b
Kiranmai Lalam <kiranmailalam@Kiranmais-MacBook-Pro.local>
2025-12-25 11:00:09 +0530

    prime factors from last

--- projectEuler/primeFactor.js
+++ projectEuler/primeFactor.js
@@ -1,24 +1,46 @@
+// function* candidates(num) {
+//   let i = 1;
+//   let x = 6 * i;
+//   yield 2;
+//   yield 3;
+//   while (x < num) {
+//     yield x - 1;
+//     yield x + 1;
+//     x = 6 * ++i;
+//   }
+// }
+
 function* candidates(num) {
-  let i = 1;
+  let i = parseInt(num / 6);
   let x = 6 * i;
-  yield 2;
-  yield 3;
-  while (x < num) {
+  while (x > 1) {
     yield x - 1;
     yield x + 1;
-    x = 6 * ++i;
+    x = 6 * --i;
   }
 }
 
-const isPrime = function (x) {
-  for (let i = 3; i <= Math.sqrt(x); i += 2) {
-    if (x % i === 0) return false;
+const isPrime = function (num) {
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) return false;
   }
   return true;
 };
 
-const number = Math.sqrt(600851475143);
-const isFactor = (factor) => 600851475143 % factor === 0;
+const isFactor = (number, factor) => number % factor === 0;
 
-const primeFactors = candidates(number).filter(isPrime).filter(isFactor);
-console.log(Math.max(...primeFactors));
+const largestPrimeFactor = (num) => {
+  const x = candidates(num);
+  let candidate = x.next().value;
+  while (candidate) {
+    if (isFactor(num, candidate) && isPrime(candidate)) {
+      return candidate;
+    }
+    candidate = x.next().value;
+  }
+};
+
+const number = 600851475143;
+// const number = 13195;
+
+console.log(largestPrimeFactor(number));
