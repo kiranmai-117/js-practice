36058d9ce25599dd4de98b7fdf9b82a24096ce76
Kiranmai Lalam <kiranmailalam@Kiranmais-MacBook-Pro.local>
2026-01-19 04:09:36 +0530

    oops tic tac toe

--- oops tic_tac_toe/src/player.js
+++ oops tic_tac_toe/src/player.js
@@ -2,12 +2,52 @@ export class player {
   constructor(name, symbol) {
     this.name = name;
     this.symbol = symbol;
+    this.position;
     this.moves = [];
+    this.winPositions = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
   }
-  recordMove(move){
+  recordMove(move) {
     this.moves.push(move);
   }
-  getMoves(){
+
+  isValid(position, ply1moves, ply2moves) {
+    return position > 0 && position < 10 && !ply1moves.includes(position) &&
+      !ply2moves.includes(position);
+  }
+
+  getMoves() {
     return this.moves.slice();
   }
+
+  getPosition(p1, p2) {
+    this.position = parseInt(prompt(this.name));
+    const ply1moves = p1.getMoves();
+    const ply2moves = p2.getMoves();
+    if (!this.isValid(this.position, ply1moves, ply2moves)) {
+      this.position = this.getPosition(p1, p2);
+    }
+    return this.position;
+  }
+
+  isWin() {
+  for (let i = 0; i < this.winPositions.length; i++) {
+    const count = this.winPositions[i].reduce((count, pos) => {
+      if (this.moves.includes(pos)) {
+        count++;
+      }
+      return count;
+    }, 0);
+    if (count === 3) return true;
+  }
+  return false;
+}
 }
--- oops tic_tac_toe/src/tic_tac_toe.js
+++ oops tic_tac_toe/src/tic_tac_toe.js
@@ -1,52 +1,14 @@
-function isWin(player) {
-  const winPositions = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-  ];
-  for (let i = 0; i < winPositions.length; i++) {
-    const count = winPositions[i].reduce((count, pos) => {
-      if (player.moves.includes(pos)) {
-        count++;
-      }
-      return count;
-    }, 0);
-    if (count === 3) return true;
-  }
-  return false;
-}
-
-export function isValid(position, ply1moves, ply2moves) {
-  return position > 0 && position < 10 && !ply1moves.includes(position) &&
-    !ply2moves.includes(position);
-}
-
-export function getPosition(player, p1, p2) {
-  let position = parseInt(prompt(player.name));
-  const ply1moves = p1.getMoves();
-  const ply2moves = p2.getMoves();
-  if (!isValid(position, ply1moves, ply2moves)) {
-    position = getPosition(player, p1, p2);
-  }
-  return position;
-}
-
 export const play = (board, p1, p2) => {
   let player = p2;
   let position;
   for (let i = 0; i < 9; i++) {
     player = (player === p1) ? p2 : p1;
-    position = getPosition(player, p1, p2);
+    position = player.getPosition(p1, p2);
     board.updateBoard(position, player.symbol);
     console.clear();
     board.display(board);
     player.recordMove(position);
-    if (isWin(player)) {
+    if (player.isWin()) {
       return `${player.name} WON`;
     }
   }
